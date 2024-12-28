fcd4eb89a7a4b3e49cddfb1f8bf4a6cbbebae096
Duy Nhor <123177857+DuyNhordzz@users.noreply.github.com>
2024-12-28 23:14:09 +0700

    baitapJavasripp

---
+++ Bai1.js
@@ -0,0 +1,8 @@
+function laSoNguyenDuong(value){
+    return (Number.isInteger(value) && value > 0);
+}
+ 
+console.log(laSoNguyenDuong(1)); // true
+console.log(laSoNguyenDuong(1.2)); // false
+console.log(laSoNguyenDuong(3)); // true
+console.log(laSoNguyenDuong(0)); // false
